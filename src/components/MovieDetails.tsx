012154af104b1a6f74c1212dcc50c3bfc135d57f
Julian Mainardis <julian.mainardis@hotmail.com>
2023-10-04 23:07:54 +0200

    Agregar Pedido button added

--- src/components/MovieDetails.tsx
+++ src/components/MovieDetails.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Text, View } from 'react-native'
+import { StyleSheet, Text, TouchableHighlight, TouchableOpacity, View } from 'react-native'
 import Icon from 'react-native-vector-icons/Ionicons';
 import { Cast } from '../interfaces/creditsInterface';
 import currencyFormatter from 'currency-formatter';
@@ -56,7 +56,31 @@ export const MovieDetails = ({movieFull, cast}: Props) => {
                     showsHorizontalScrollIndicator={false}
                     style={{marginTop: 10, height: 70}}
                 />
+                <View style={{width: 200, height: 100, alignSelf: 'center'}}>
+                    <TouchableOpacity activeOpacity={0.7}>
+                        <View style={styles.buttonContainer}>
+                            <Text style={{fontSize: 20, fontWeight: 'bold', marginVertical: 5}}>Agregar al pedido</Text>
+                        </View>
+                    </TouchableOpacity>
+                </View>
             </View>
         </>
     )
 }
+
+const styles = StyleSheet.create({
+    buttonContainer: {
+        alignItems: 'center',
+        marginTop: 20,
+        backgroundColor: 'white',
+        borderRadius: 30,
+        shadowColor: "#000",
+        shadowOffset: {
+            width: 0,
+            height: 10,
+        },
+        shadowOpacity: 0.24,
+        shadowRadius: 23,
+        elevation: 3,
+    }
+});
